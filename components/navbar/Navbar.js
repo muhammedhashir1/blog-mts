144b5119df7d98ae20de5751d1c736ea9748a1dc
muhammed hashir <hashiraachi818@gmail.com>
2023-11-26 10:36:24 +0530

    icon size increased

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav className="bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 p-4 flex justify-between items-center text-white font-semibold">
       <div className="flex items-center space-x-2">
-        <Image src="/maple.png" width={30} height={15} alt="Logo" />
+        <Image src="/maple.png" width={40} height={22} alt="Logo" />
       </div>
 
       <div className="hidden md:inline font-light" style={{ fontFamily: "Poppins, sans-serif" }}>
